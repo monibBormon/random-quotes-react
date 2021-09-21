d72ff64645369264c7ab5fb809a4be9768a533f4
monibBormon <monibbormon14@gmail.com>
2021-09-22 00:45:51 +0600

    random user api

---
+++ src/components/Quotes/Quotes.js
@@ -0,0 +1,30 @@
+
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import './Quotes.css'
+
+const Quotes = () => {
+    const [quote, setQuote] = useState([])
+    useEffect(() => {
+        fetch('https://api.kanye.rest/')
+            .then(res => res.json())
+            .then(data => setQuote(data.quote))
+    }, [])
+    const loadNext = () => {
+        fetch('https://api.kanye.rest/')
+            .then(res => res.json())
+            .then(data => setQuote(data.quote))
+    }
+    return (
+        <div>
+            <div className="quote">
+                <div className="card">
+                    <h3>" {quote} "</h3>
+                    <button onClick={loadNext}><span>Another Quote</span></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Quotes;
